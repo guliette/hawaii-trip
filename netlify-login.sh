a4448fb96c06ad88392fa01e065628b1811cf387
Juliette <juliettedu@Juliettes-MacBook-Pro.local>
2026-07-22 01:37:23 +0800

    Remove temp/preview files, re-add title images

--- netlify-login.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Opening Netlify Drop in browser..."
-open "https://app.netlify.com/drop"
-echo ""
-echo "Instructions:"
-echo "1. Drag hawaii-v2.html + img/ folder + leaflet/ folder onto the page"
-echo "2. Wait 5 seconds, copy the URL"
-echo "3. Share that URL with your friends"
